935888098a841535aa450f803b64f6a7dad949f2
DhanuXworkz <dhananjayks.xworkz@gmail.com>
2024-11-13 18:03:22 +0530

    first commit

---
+++ StudentsDetails.java
@@ -0,0 +1,352 @@
+public class StudentsDetails{
+public static void main(String args[]){
+// Array creation using literals
+        int []id = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        char []grade = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        short []ages = {10, 12, 23, 24, 25, 26, 27, 21, 90, 20};
+        long []mobileNo = {90800L, 20800L, 38000L, 40870L, 57600L, 68600L, 70230L, 80430L, 93500L, 100350L};
+        float []percentage = {82.1f, 82.2f, 83.3f, 84.4f, 85.5f, 86.6f, 87.7f, 88.8f, 89.9f, 80.1f};
+        double[] height = {5.01, 5.02, 5.03, 4.04, 5.05, 6.06, 5.07, 5.08, 5.09, 6.1};
+        String[] name = {"Dhanu", "adi", "gowth", "name1", "name2", "name3", "name4", "name5", "name6", "name7"};
+        boolean[] result = {true, false, true, false, true, false, true, false, true, false};
+        byte[] backlogs = {1, 2, 1, 2, 3, 4, 6, 0, 1, 2};
+// array id 
+System.out.println("\nstudent id: " + id[0]);
+System.out.println("student id: " + id[1]);
+System.out.println("student id: " + id[2]);
+System.out.println("student id: " + id[3]);
+System.out.println("student id: " + id[4]);
+System.out.println("student id: " + id[5]);
+System.out.println("student id: " + id[6]);
+System.out.println("student id: " + id[7]);
+System.out.println("student id: " + id[8]);
+System.out.println("student id: " + id[9]);
+
+//grade array
+System.out.println("\n student grade: " + grade[0]);
+System.out.println("student grade: " + grade[1]);
+System.out.println("student grade: " + grade[2]);
+System.out.println("student grade: " + grade[3]);
+System.out.println("student grade: " + grade[4]);
+System.out.println("student grade: " + grade[5]);
+System.out.println("student grade: " + grade[6]);
+System.out.println("student grade: " + grade[7]);
+System.out.println("student grade: " + grade[8]);
+System.out.println("student grade: " + grade[9]);
+
+// ages
+
+System.out.println("\nstudent ages: " + ages[0]);
+System.out.println("student ages: " + ages[1]);
+System.out.println("student ages: " + ages[2]);
+System.out.println("student ages: " + ages[3]);
+System.out.println("student ages: " + ages[4]);
+System.out.println("student ages: " + ages[5]);
+System.out.println("student ages: " + ages[6]);
+System.out.println("student ages: " + ages[7]);
+System.out.println("student ages: " + ages[8]);
+System.out.println("student ages: " + ages[9]);
+
+//mobileNo
+
+System.out.println("\nstudent mobileNo: " + mobileNo[0]);
+System.out.println("student mobileNo: " + mobileNo[1]);
+System.out.println("student mobileNo: " + mobileNo[2]);
+System.out.println("student mobileNo: " + mobileNo[3]);
+System.out.println("student mobileNo: " + mobileNo[4]);
+System.out.println("student mobileNo: " + mobileNo[5]);
+System.out.println("student mobileNo: " + mobileNo[6]);
+System.out.println("student mobileNo: " + mobileNo[7]);
+System.out.println("student mobileNo: " + mobileNo[8]);
+System.out.println("student mobileNo: " + mobileNo[9]);
+
+//pecentage 
+System.out.println("\n student percentage: " + percentage[0]);
+System.out.println("student percentage: " + percentage[1]);
+System.out.println("student percentage: " + percentage[2]);
+System.out.println("student percentage: " + percentage[3]);
+System.out.println("student percentage: " + percentage[4]);
+System.out.println("student percentage: " + percentage[5]);
+System.out.println("student percentage: " + percentage[6]);
+System.out.println("student percentage: " + percentage[7]);
+System.out.println("student percentage: " + percentage[8]);
+System.out.println("student percentage: " + percentage[9]);
+
+//height
+System.out.println("\nstudent height: " + height[0]);
+System.out.println("student height: " + height[1]);
+System.out.println("student height: " + height[2]);
+System.out.println("student height: " + height[3]);
+System.out.println("student height: " + height[4]);
+System.out.println("student height: " + height[5]);
+System.out.println("student height: " + height[6]);
+System.out.println("student height: " + height[7]);
+System.out.println("student height: " + height[8]);
+System.out.println("student height: " + height[9]);
+
+//name
+
+System.out.println("\nstudent name: " + name[0]);
+System.out.println("student name: " + name[1]);
+System.out.println("student name: " + name[2]);
+System.out.println("student name: " + name[3]);
+System.out.println("student name: " + name[4]);
+System.out.println("student name: " + name[5]);
+System.out.println("student name: " + name[6]);
+System.out.println("student name: " + name[7]);
+System.out.println("student name: " + name[8]);
+System.out.println("student name: " + name[9]);
+
+//result
+System.out.println("\nstudent result: " + result[0]);
+System.out.println("student result: " + result[1]);
+System.out.println("student result: " + result[2]);
+System.out.println("student result: " + result[3]);
+System.out.println("student result: " + result[4]);
+System.out.println("student result: " + result[5]);
+System.out.println("student result: " + result[6]);
+System.out.println("student result: " + result[7]);
+System.out.println("student result: " + result[8]);
+System.out.println("student result: " + result[9]);
+
+// backlogs
+System.out.println("\nstudent backlogs: " + backlogs[0]);
+System.out.println("student backlogs: " + backlogs[1]);
+System.out.println("student backlogs: " + backlogs[2]);
+System.out.println("student backlogs: " + backlogs[3]);
+System.out.println("student backlogs: " + backlogs[4]);
+System.out.println("student backlogs: " + backlogs[5]);
+System.out.println("student backlogs: " + backlogs[6]);
+System.out.println("student backlogs: " + backlogs[7]);
+System.out.println("student backlogs: " + backlogs[8]);
+System.out.println("student backlogs: " + backlogs[9]);
+
+//reasiign the element
+System.out.println("\nreassign the array elements");
+id[0] = 2;
+id[1] = 4;
+id[2] = 6;
+id[3] = 8;
+id[4] = 10;
+id[5] = 12;
+id[6] = 14;
+id[7] = 16;
+id[8] = 18;
+id[9] = 20;
+
+System.out.println("\nstudent id: " + id[0]);
+System.out.println("student id: " + id[1]);
+System.out.println("student id: " + id[2]);
+System.out.println("student id: " + id[3]);
+System.out.println("student id: " + id[4]);
+System.out.println("student id: " + id[5]);
+System.out.println("student id: " + id[6]);
+System.out.println("student id: " + id[7]);
+System.out.println("student id: " + id[8]);
+System.out.println("student id: " + id[9]);
+
+
+grade[0] = 'z';
+grade[1] = 'y';
+grade[2] = 'x';
+grade[3] = 'w';
+grade[4] = 'v';
+grade[5] = 'u';
+grade[6] = 't';
+grade[7] = 's';
+grade[8] = 'r';
+grade[9] = 'q';
+
+ages[0] = 20;
+ages[1] = 22;
+ages[2] = 30;
+ages[3] = 31;
+ages[4] = 32;
+ages[5] = 33;
+ages[6] = 34;
+ages[7] = 25;
+ages[8] = 35;
+ages[9] = 40;
+
+mobileNo[0] = 12345L;
+mobileNo[1] = 67890L;
+mobileNo[2] = 11223L;
+mobileNo[3] = 44556L;
+mobileNo[4] = 78901L;
+mobileNo[5] = 23456L;
+mobileNo[6] = 34567L;
+mobileNo[7] = 45678L;
+mobileNo[8] = 56789L;
+mobileNo[9] = 67890L;
+
+percentage[0] = 90.1f;
+percentage[1] = 91.2f;
+percentage[2] = 92.3f;
+percentage[3] = 93.4f;
+percentage[4] = 94.5f;
+percentage[5] = 95.6f;
+percentage[6] = 96.7f;
+percentage[7] = 97.8f;
+percentage[8] = 98.9f;
+percentage[9] = 99.0f;
+
+height[0] = 5.5;
+height[1] = 5.6;
+height[2] = 5.7;
+height[3] = 4.8;
+height[4] = 5.9;
+height[5] = 6.0;
+height[6] = 5.3;
+height[7] = 5.4;
+height[8] = 5.2;
+height[9] = 6.1;
+
+name[0] = "Name1";
+name[1] = "Name2";
+name[2] = "Name3";
+name[3] = "Name4";
+name[4] = "Name5";
+name[5] = "Name6";
+name[6] = "Name7";
+name[7] = "Name8";
+name[8] = "Name9";
+name[9] = "Name10";
+
+result[0] = false;
+result[1] = true;
+result[2] = false;
+result[3] = true;
+result[4] = false;
+result[5] = true;
+result[6] = false;
+result[7] = true;
+result[8] = false;
+result[9] = true;
+
+backlogs[0] = 2;
+backlogs[1] = 3;
+backlogs[2] = 1;
+backlogs[3] = 4;
+backlogs[4] = 2;
+backlogs[5] = 3;
+backlogs[6] = 5;
+backlogs[7] = 0;
+backlogs[8] = 1;
+backlogs[9] = 2;
+
+
+System.out.println("\n student grade: " + grade[0]);
+System.out.println("student grade: " + grade[1]);
+System.out.println("student grade: " + grade[2]);
+System.out.println("student grade: " + grade[3]);
+System.out.println("student grade: " + grade[4]);
+System.out.println("student grade: " + grade[5]);
+System.out.println("student grade: " + grade[6]);
+System.out.println("student grade: " + grade[7]);
+System.out.println("student grade: " + grade[8]);
+System.out.println("student grade: " + grade[9]);
+
+//grade array
+System.out.println("\n student grade: " + grade[0]);
+System.out.println("student grade: " + grade[1]);
+System.out.println("student grade: " + grade[2]);
+System.out.println("student grade: " + grade[3]);
+System.out.println("student grade: " + grade[4]);
+System.out.println("student grade: " + grade[5]);
+System.out.println("student grade: " + grade[6]);
+System.out.println("student grade: " + grade[7]);
+System.out.println("student grade: " + grade[8]);
+System.out.println("student grade: " + grade[9]);
+
+// ages
+
+System.out.println("\nstudent ages: " + ages[0]);
+System.out.println("student ages: " + ages[1]);
+System.out.println("student ages: " + ages[2]);
+System.out.println("student ages: " + ages[3]);
+System.out.println("student ages: " + ages[4]);
+System.out.println("student ages: " + ages[5]);
+System.out.println("student ages: " + ages[6]);
+System.out.println("student ages: " + ages[7]);
+System.out.println("student ages: " + ages[8]);
+System.out.println("student ages: " + ages[9]);
+
+//mobileNo
+
+System.out.println("\nstudent mobileNo: " + mobileNo[0]);
+System.out.println("student mobileNo: " + mobileNo[1]);
+System.out.println("student mobileNo: " + mobileNo[2]);
+System.out.println("student mobileNo: " + mobileNo[3]);
+System.out.println("student mobileNo: " + mobileNo[4]);
+System.out.println("student mobileNo: " + mobileNo[5]);
+System.out.println("student mobileNo: " + mobileNo[6]);
+System.out.println("student mobileNo: " + mobileNo[7]);
+System.out.println("student mobileNo: " + mobileNo[8]);
+System.out.println("student mobileNo: " + mobileNo[9]);
+
+//pecentage 
+System.out.println("\n student percentage: " + percentage[0]);
+System.out.println("student percentage: " + percentage[1]);
+System.out.println("student percentage: " + percentage[2]);
+System.out.println("student percentage: " + percentage[3]);
+System.out.println("student percentage: " + percentage[4]);
+System.out.println("student percentage: " + percentage[5]);
+System.out.println("student percentage: " + percentage[6]);
+System.out.println("student percentage: " + percentage[7]);
+System.out.println("student percentage: " + percentage[8]);
+System.out.println("student percentage: " + percentage[9]);
+
+//height
+System.out.println("\nstudent height: " + height[0]);
+System.out.println("student height: " + height[1]);
+System.out.println("student height: " + height[2]);
+System.out.println("student height: " + height[3]);
+System.out.println("student height: " + height[4]);
+System.out.println("student height: " + height[5]);
+System.out.println("student height: " + height[6]);
+System.out.println("student height: " + height[7]);
+System.out.println("student height: " + height[8]);
+System.out.println("student height: " + height[9]);
+
+//name
+
+System.out.println("\nstudent name: " + name[0]);
+System.out.println("student name: " + name[1]);
+System.out.println("student name: " + name[2]);
+System.out.println("student name: " + name[3]);
+System.out.println("student name: " + name[4]);
+System.out.println("student name: " + name[5]);
+System.out.println("student name: " + name[6]);
+System.out.println("student name: " + name[7]);
+System.out.println("student name: " + name[8]);
+System.out.println("student name: " + name[9]);
+
+//result
+System.out.println("\nstudent result: " + result[0]);
+System.out.println("student result: " + result[1]);
+System.out.println("student result: " + result[2]);
+System.out.println("student result: " + result[3]);
+System.out.println("student result: " + result[4]);
+System.out.println("student result: " + result[5]);
+System.out.println("student result: " + result[6]);
+System.out.println("student result: " + result[7]);
+System.out.println("student result: " + result[8]);
+System.out.println("student result: " + result[9]);
+
+// backlogs
+System.out.println("\nstudent backlogs: " + backlogs[0]);
+System.out.println("student backlogs: " + backlogs[1]);
+System.out.println("student backlogs: " + backlogs[2]);
+System.out.println("student backlogs: " + backlogs[3]);
+System.out.println("student backlogs: " + backlogs[4]);
+System.out.println("student backlogs: " + backlogs[5]);
+System.out.println("student backlogs: " + backlogs[6]);
+System.out.println("student backlogs: " + backlogs[7]);
+System.out.println("student backlogs: " + backlogs[8]);
+System.out.println("student backlogs: " + backlogs[9]);
+
+
+
+}
+
+}
---
+++ StudentsDetailsOne.java
@@ -0,0 +1,347 @@
+public class StudentsDetailsOne{
+public static void main(String args[]){
+
+// array creation  using  new key word
+
+int id[] =new int[10];
+ char []grade= new char [10];
+ short []ages =new short[10];
+ long []mobileNo=new long [10];
+ float []percentage= new float[10];
+ double[] height=new double[10];
+ String name[]= new String[10];
+  boolean[] result= new boolean [10];
+  byte[] backlogs=new byte[10];
+  
+  // array id 
+System.out.println("\nstudent id: " + id[0]);
+System.out.println("student id: " + id[1]);
+System.out.println("student id: " + id[2]);
+System.out.println("student id: " + id[3]);
+System.out.println("student id: " + id[4]);
+System.out.println("student id: " + id[5]);
+System.out.println("student id: " + id[6]);
+System.out.println("student id: " + id[7]);
+System.out.println("student id: " + id[8]);
+System.out.println("student id: " + id[9]);
+
+//grade array
+System.out.println("\n student grade: " + grade[0]);
+System.out.println("student grade: " + grade[1]);
+System.out.println("student grade: " + grade[2]);
+System.out.println("student grade: " + grade[3]);
+System.out.println("student grade: " + grade[4]);
+System.out.println("student grade: " + grade[5]);
+System.out.println("student grade: " + grade[6]);
+System.out.println("student grade: " + grade[7]);
+System.out.println("student grade: " + grade[8]);
+System.out.println("student grade: " + grade[9]);
+
+// ages
+
+System.out.println("\nstudent ages: " + ages[0]);
+System.out.println("student ages: " + ages[1]);
+System.out.println("student ages: " + ages[2]);
+System.out.println("student ages: " + ages[3]);
+System.out.println("student ages: " + ages[4]);
+System.out.println("student ages: " + ages[5]);
+System.out.println("student ages: " + ages[6]);
+System.out.println("student ages: " + ages[7]);
+System.out.println("student ages: " + ages[8]);
+System.out.println("student ages: " + ages[9]);
+
+//mobileNo
+
+System.out.println("\nstudent mobileNo: " + mobileNo[0]);
+System.out.println("student mobileNo: " + mobileNo[1]);
+System.out.println("student mobileNo: " + mobileNo[2]);
+System.out.println("student mobileNo: " + mobileNo[3]);
+System.out.println("student mobileNo: " + mobileNo[4]);
+System.out.println("student mobileNo: " + mobileNo[5]);
+System.out.println("student mobileNo: " + mobileNo[6]);
+System.out.println("student mobileNo: " + mobileNo[7]);
+System.out.println("student mobileNo: " + mobileNo[8]);
+System.out.println("student mobileNo: " + mobileNo[9]);
+
+//pecentage 
+System.out.println("\n student percentage: " + percentage[0]);
+System.out.println("student percentage: " + percentage[1]);
+System.out.println("student percentage: " + percentage[2]);
+System.out.println("student percentage: " + percentage[3]);
+System.out.println("student percentage: " + percentage[4]);
+System.out.println("student percentage: " + percentage[5]);
+System.out.println("student percentage: " + percentage[6]);
+System.out.println("student percentage: " + percentage[7]);
+System.out.println("student percentage: " + percentage[8]);
+System.out.println("student percentage: " + percentage[9]);
+
+//height
+System.out.println("\nstudent height: " + height[0]);
+System.out.println("student height: " + height[1]);
+System.out.println("student height: " + height[2]);
+System.out.println("student height: " + height[3]);
+System.out.println("student height: " + height[4]);
+System.out.println("student height: " + height[5]);
+System.out.println("student height: " + height[6]);
+System.out.println("student height: " + height[7]);
+System.out.println("student height: " + height[8]);
+System.out.println("student height: " + height[9]);
+
+//name
+
+System.out.println("\nstudent name: " + name[0]);
+System.out.println("student name: " + name[1]);
+System.out.println("student name: " + name[2]);
+System.out.println("student name: " + name[3]);
+System.out.println("student name: " + name[4]);
+System.out.println("student name: " + name[5]);
+System.out.println("student name: " + name[6]);
+System.out.println("student name: " + name[7]);
+System.out.println("student name: " + name[8]);
+System.out.println("student name: " + name[9]);
+
+//result
+System.out.println("\nstudent result: " + result[0]);
+System.out.println("student result: " + result[1]);
+System.out.println("student result: " + result[2]);
+System.out.println("student result: " + result[3]);
+System.out.println("student result: " + result[4]);
+System.out.println("student result: " + result[5]);
+System.out.println("student result: " + result[6]);
+System.out.println("student result: " + result[7]);
+System.out.println("student result: " + result[8]);
+System.out.println("student result: " + result[9]);
+
+// backlogs
+System.out.println("\nstudent backlogs: " + backlogs[0]);
+System.out.println("student backlogs: " + backlogs[1]);
+System.out.println("student backlogs: " + backlogs[2]);
+System.out.println("student backlogs: " + backlogs[3]);
+System.out.println("student backlogs: " + backlogs[4]);
+System.out.println("student backlogs: " + backlogs[5]);
+System.out.println("student backlogs: " + backlogs[6]);
+System.out.println("student backlogs: " + backlogs[7]);
+System.out.println("student backlogs: " + backlogs[8]);
+System.out.println("student backlogs: " + backlogs[9]);
+
+// re-assign the elements
+System.out.println("\n\nreassigning elements ");
+
+id[0]= 1;
+id[0] = 1;
+id[1] = 2;
+id[2] = 3;
+id[3] = 4;
+id[4] = 5;
+id[5] = 6;
+id[6] = 7;
+id[7] = 8;
+id[8] = 9;
+id[9] = 10;
+
+grade[0] = 'a';
+grade[1] = 'b';
+grade[2] = 'c';
+grade[3] = 'd';
+grade[4] = 'e';
+grade[5] = 'f';
+grade[6] = 'g';
+grade[7] = 'h';
+grade[8] = 'i';
+grade[9] = 'j';
+
+ages[0] = 10;
+ages[1] = 12;
+ages[2] = 14;
+ages[3] = 16;
+ages[4] = 18;
+ages[5] = 20;
+ages[6] = 22;
+ages[7] = 24;
+ages[8] = 26;
+ages[9] = 28;
+
+
+mobileNo[0] = 90800L;
+mobileNo[1] = 20800L;
+mobileNo[2] = 38000L;
+mobileNo[3] = 40870L;
+mobileNo[4] = 57600L;
+mobileNo[5] = 68600L;
+mobileNo[6] = 70230L;
+mobileNo[7] = 80430L;
+mobileNo[8] = 93500L;
+mobileNo[9] = 100350L;
+
+percentage[0] = 82.1f;
+percentage[1] = 83.2f;
+percentage[2] = 84.3f;
+percentage[3] = 85.4f;
+percentage[4] = 86.5f;
+percentage[5] = 87.6f;
+percentage[6] = 88.7f;
+percentage[7] = 89.8f;
+percentage[8] = 90.9f;
+percentage[9] = 91.0f;
+
+height[0] = 5.01;
+height[1] = 5.02;
+height[2] = 5.03;
+height[3] = 5.04;
+height[4] = 5.05;
+height[5] = 5.06;
+height[6] = 5.07;
+height[7] = 5.08;
+height[8] = 5.09;
+height[9] = 5.10;
+
+name[0] = "name1 ";
+name[1] = "name2";
+name[2] = "name0";
+name[3] = "name3";
+name[4] = "name4";
+name[5] = "name5";
+name[6] = "name6";
+name[7] = "name7";
+name[8] = "name4";
+name[9] = "Ivy";
+
+result[0] = true;
+result[1] = false;
+result[2] = true;
+result[3] = false;
+result[4] = true;
+result[5] = false;
+result[6] = true;
+result[7] = false;
+result[8] = true;
+result[9] = false;
+
+
+backlogs[0] = 1;
+backlogs[1] = 2;
+backlogs[2] = 3;
+backlogs[3] = 4;
+backlogs[4] = 5;
+backlogs[5] = 6;
+backlogs[6] = 7;
+backlogs[7] = 8;
+backlogs[8] = 9;
+backlogs[9] = 10;
+
+// array id 
+System.out.println("\nstudent id: " + id[0]);
+System.out.println("student id: " + id[1]);
+System.out.println("student id: " + id[2]);
+System.out.println("student id: " + id[3]);
+System.out.println("student id: " + id[4]);
+System.out.println("student id: " + id[5]);
+System.out.println("student id: " + id[6]);
+System.out.println("student id: " + id[7]);
+System.out.println("student id: " + id[8]);
+System.out.println("student id: " + id[9]);
+
+//grade array
+System.out.println("\n student grade: " + grade[0]);
+System.out.println("student grade: " + grade[1]);
+System.out.println("student grade: " + grade[2]);
+System.out.println("student grade: " + grade[3]);
+System.out.println("student grade: " + grade[4]);
+System.out.println("student grade: " + grade[5]);
+System.out.println("student grade: " + grade[6]);
+System.out.println("student grade: " + grade[7]);
+System.out.println("student grade: " + grade[8]);
+System.out.println("student grade: " + grade[9]);
+
+// ages
+
+System.out.println("\nstudent ages: " + ages[0]);
+System.out.println("student ages: " + ages[1]);
+System.out.println("student ages: " + ages[2]);
+System.out.println("student ages: " + ages[3]);
+System.out.println("student ages: " + ages[4]);
+System.out.println("student ages: " + ages[5]);
+System.out.println("student ages: " + ages[6]);
+System.out.println("student ages: " + ages[7]);
+System.out.println("student ages: " + ages[8]);
+System.out.println("student ages: " + ages[9]);
+
+//mobileNo
+
+System.out.println("\nstudent mobileNo: " + mobileNo[0]);
+System.out.println("student mobileNo: " + mobileNo[1]);
+System.out.println("student mobileNo: " + mobileNo[2]);
+System.out.println("student mobileNo: " + mobileNo[3]);
+System.out.println("student mobileNo: " + mobileNo[4]);
+System.out.println("student mobileNo: " + mobileNo[5]);
+System.out.println("student mobileNo: " + mobileNo[6]);
+System.out.println("student mobileNo: " + mobileNo[7]);
+System.out.println("student mobileNo: " + mobileNo[8]);
+System.out.println("student mobileNo: " + mobileNo[9]);
+
+//pecentage 
+System.out.println("\n student percentage: " + percentage[0]);
+System.out.println("student percentage: " + percentage[1]);
+System.out.println("student percentage: " + percentage[2]);
+System.out.println("student percentage: " + percentage[3]);
+System.out.println("student percentage: " + percentage[4]);
+System.out.println("student percentage: " + percentage[5]);
+System.out.println("student percentage: " + percentage[6]);
+System.out.println("student percentage: " + percentage[7]);
+System.out.println("student percentage: " + percentage[8]);
+System.out.println("student percentage: " + percentage[9]);
+
+//height
+System.out.println("\nstudent height: " + height[0]);
+System.out.println("student height: " + height[1]);
+System.out.println("student height: " + height[2]);
+System.out.println("student height: " + height[3]);
+System.out.println("student height: " + height[4]);
+System.out.println("student height: " + height[5]);
+System.out.println("student height: " + height[6]);
+System.out.println("student height: " + height[7]);
+System.out.println("student height: " + height[8]);
+System.out.println("student height: " + height[9]);
+
+//name
+
+System.out.println("\nstudent name: " + name[0]);
+System.out.println("student name: " + name[1]);
+System.out.println("student name: " + name[2]);
+System.out.println("student name: " + name[3]);
+System.out.println("student name: " + name[4]);
+System.out.println("student name: " + name[5]);
+System.out.println("student name: " + name[6]);
+System.out.println("student name: " + name[7]);
+System.out.println("student name: " + name[8]);
+System.out.println("student name: " + name[9]);
+
+//result
+System.out.println("\nstudent result: " + result[0]);
+System.out.println("student result: " + result[1]);
+System.out.println("student result: " + result[2]);
+System.out.println("student result: " + result[3]);
+System.out.println("student result: " + result[4]);
+System.out.println("student result: " + result[5]);
+System.out.println("student result: " + result[6]);
+System.out.println("student result: " + result[7]);
+System.out.println("student result: " + result[8]);
+System.out.println("student result: " + result[9]);
+
+// backlogs
+System.out.println("\nstudent backlogs: " + backlogs[0]);
+System.out.println("student backlogs: " + backlogs[1]);
+System.out.println("student backlogs: " + backlogs[2]);
+System.out.println("student backlogs: " + backlogs[3]);
+System.out.println("student backlogs: " + backlogs[4]);
+System.out.println("student backlogs: " + backlogs[5]);
+System.out.println("student backlogs: " + backlogs[6]);
+System.out.println("student backlogs: " + backlogs[7]);
+System.out.println("student backlogs: " + backlogs[8]);
+System.out.println("student backlogs: " + backlogs[9]);
+
+
+
+}
+
+}
