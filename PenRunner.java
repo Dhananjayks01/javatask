ef6f0559d1e24dafb2a063a95fb3c22679440353
DhanuXworkz <dhananjayks.xworkz@gmail.com>
2024-11-15 00:11:17 +0530

    java task on constructor

---
+++ PenRunner.java
@@ -0,0 +1,96 @@
+public class PenRunner{
+
+public static void main(String args[]){
+
+Pen pen=new Pen();
+
+pen.penName="Parker";
+pen.color= "Silver";
+pen.price=520;
+pen.penType="Ball pen";
+pen.wieght=6.7f;
+pen.inkColor="Black";
+
+Pen pen1=new Pen();
+
+pen1.penName="Doms";
+pen1.color= "Red";
+pen1.price=20;
+pen1.penType="Ball pen";
+pen1.wieght=7.7f;
+pen1.inkColor="Blue";
+
+
+Pen pen2=new Pen();
+
+pen2.penName="Cello";
+pen2.color= "blue";
+pen2.price=10;
+pen2.penType="Ball pen";
+pen2.wieght=6.7f;
+pen2.inkColor="blue";
+
+
+Pen pen3=new Pen();
+
+pen3.penName="renaltz brite";
+pen3.color= "Black";
+pen3.price=5;
+pen3.penType="Ball pen";
+pen3.wieght=6.9f;
+pen3.inkColor="Black";
+
+Pen pen4=new Pen();
+
+pen4.penName="pentonic";
+pen4.color= "red";
+pen4.price=10;
+pen4.penType="Gel pen";
+pen4.wieght=6.7f;
+pen4.inkColor="red";
+
+
+
+
+
+
+System.out.println(pen.penName);
+System.out.println(pen.color);
+System.out.println(pen.price);
+System.out.println(pen.penType);
+System.out.println(pen.wieght);
+System.out.println(pen.inkColor);
+System.out.println("\n"+pen1.penName);
+System.out.println(pen1.color);
+System.out.println(pen1.price);
+System.out.println(pen1.penType);
+System.out.println(pen1.wieght);
+System.out.println(pen1.inkColor);
+System.out.println("\n"+pen2.penName);
+System.out.println(pen2.color);
+System.out.println(pen2.price);
+System.out.println(pen2.penType);
+System.out.println(pen2.wieght);
+System.out.println(pen2.inkColor);
+
+System.out.println("\n"+pen3.penName);
+System.out.println(pen3.color);
+System.out.println(pen3.price);
+System.out.println(pen3.penType);
+System.out.println(pen3.wieght);
+System.out.println(pen3.inkColor);
+
+System.out.println("\n"+pen4.penName);
+System.out.println(pen4.color);
+System.out.println(pen4.price);
+System.out.println(pen4.penType);
+System.out.println(pen4.wieght);
+System.out.println(pen4.inkColor);
+
+
+
+
+}
+
+
+}
