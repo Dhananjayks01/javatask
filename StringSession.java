b2fe831e8c8bc83dcf82e079669801f1c13bf8bf
DhanuXworkz <dhananjayks.xworkz@gmail.com>
2024-11-27 02:25:20 +0530

    String methods

---
+++ StringSession.java
@@ -0,0 +1,14 @@
+public class StringSession{
+
+public static  void main(String args[]){
+String word ="Good Morning"; //using literals
+String message ="Good Morning";
+
+System.out.println(word ==message);
+
+message="Evening";
+System.out.println(word=message);
+
+
+}
+}
